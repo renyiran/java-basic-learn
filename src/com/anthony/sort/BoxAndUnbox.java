2288543bfae0ea95403512547ccabbb1bfff7c22
Anthony <373133745@qq.com>
2019-02-23 23:27:09 +0800

    装箱与拆箱

---
+++ src/com/anthony/sort/BoxAndUnbox.java
@@ -0,0 +1,35 @@
+package com.anthony.sort;
+
+
+/**
+ * 装箱与拆箱（龙鸣英语 将就将就）
+ */
+public class BoxAndUnbox {
+    public static void main(String[] args) {
+
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        Long h = 2L;
+
+        System.out.println(c == d);
+        System.out.println(e == f);
+        System.out.println(c == (a + b));
+        System.out.println(c.equals(a + b));
+        System.out.println(g == (a + b));
+        System.out.println(g.equals(a + b));
+        System.out.println(g.equals(a + h));
+        /**
+         * 在通过valueOf方法创建Integer对象的时候，如果数值在[-128,127]之间，便返回指向IntegerCache.cache中已经存在的对象的引用；否则创建一个新的Integer对象。
+         * 上面的代码中i1和i2的数值为100，因此会直接从cache中取已经存在的对象，所以i1和i2指向的是同一个对象，而i3和i4则是分别指向不同的对象
+         * 第三句由于  a+b包含了算术运算，因此会触发自动拆箱过程（会调用intValue方法），因此它们比较的是数值是否相等。
+         * 而对于c.equals(a+b)会先触发自动拆箱过程，再触发自动装箱过程，也就是说a+b，会先各自调用intValue方法，得到了加法运算后的数值之后，
+         * 便调用Integer.valueOf方法，再进行equals比较。
+         * 同理对于后面的也是这样，不过要注意倒数第二个和最后一个输出的结果（如果数值是int类型的，装箱过程调用的是Integer.valueOf；如果是long类型的，装箱调用的Long.valueOf方法）。
+         */
+    }
+}
